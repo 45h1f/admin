3fa00af64b198013fd56003f23f4a7c9b5094c49
Ashif <ashiful134@gmail.com>
2023-03-26 01:38:03 +0600

    update

--- src/extensions/GridSortable/routes/web.php
+++ src/extensions/GridSortable/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use Ashiful\Extensions\GridSortable\Controllers\GridSortableController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('_grid-sortable_', [GridSortableController::class, 'sort'])->name('laravel-admin-grid-sortable');
